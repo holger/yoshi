3e08eca1a8b650ba87cf1ba3b067f059165c0f22
Holger Rüprich <holger@rueprich.de>
2012-04-05 22:09:30 +0200

    Refactored Route and fixed bug with path parameter

--- src/yoshi/Route.php
+++ src/yoshi/Route.php
@@ -11,7 +11,7 @@ class Route {
   
   public function __construct($method, $path, $callback) {
     $path_pattern = str_replace('/', '\/', $path);
-    $path_pattern = preg_replace('/[{][^}]*[}]/', '([^\/]*)', $path_pattern);
+    $path_pattern = preg_replace('/[{][^}]*[}]/', '([^\/]+)', $path_pattern);
     
     $this->compiled_path = '/^' . $method . '#' . $path_pattern . '$/i';
     $this->path = $path;
@@ -19,20 +19,23 @@ class Route {
     $this->callback = $callback;
   }
   
-  public function matches(Request $request, $base_path) {
-    $path = str_replace($base_path, '', $request->getRequestUriPath());
-    $method = $request->getRequestMethod();
-  
-    return preg_match($this->compiled_path, $method . '#' . $path);
+  public function matches(Request $request, $base_path = '') {
+    return count($this->_matches($request, $base_path)) > 0;
   }
   
-  public function execute(Request $request, $base_path) {
+  public function execute(Request $request, $base_path = '') {
+    if (count($matches = $this->_matches($request, $base_path)) > 0) {
+      array_shift($matches);
+      return call_user_func_array($this->callback, $matches);
+    }
+  }
+  
+  private function _matches(Request $request, $base_path) {
     $path = str_replace($base_path, '', $request->getRequestUriPath());
     $method = $request->getRequestMethod();
-    
-    preg_match($this->compiled_path, $method . '#' . $path, $arguments);
-    array_shift($arguments);
-    return call_user_func_array($this->callback, $arguments);
+  
+    preg_match($this->compiled_path, $method . '#' . $path, $matches);
+    return $matches;
   }
 
 }
