1776a40e8576ebf7635f4806bc40d0ef89d62336
Holger Rüprich <holger@rueprich.de>
2012-04-20 23:02:32 +0200

    Renamed addRoute to route

--- src/yoshi/Router.php
+++ src/yoshi/Router.php
@@ -10,36 +10,34 @@ class Router {
   private $routes = array();
   
   public function get($path, $callback, $callback_method = null) {
-    $this->addRoute('GET', $path, $callback, $callback_method);
+    $this->route('GET', $path, $callback, $callback_method);
   }
   
   public function post($path, $callback, $callback_method = null) {
-    $this->addRoute('POST', $path, $callback, $callback_method);
+    $this->route('POST', $path, $callback, $callback_method);
   }
   
   public function put($path, $callback, $callback_method = null) {
-    $this->addRoute('PUT', $path, $callback, $callback_method);
+    $this->route('PUT', $path, $callback, $callback_method);
   }
   
   public function delete($path, $callback, $callback_method = null) {
-    $this->addRoute('DELETE', $path, $callback, $callback_method);
+    $this->route('DELETE', $path, $callback, $callback_method);
   }
   
   public function head($path, $callback, $callback_method = null) {
-    $this->addRoute('HEAD', $path, $callback, $callback_method);
+    $this->route('HEAD', $path, $callback, $callback_method);
   }
   
   public function options($path, $callback, $callback_method = null) {
-    $this->addRoute('OPTIONS', $path, $callback, $callback_method);
+    $this->route('OPTIONS', $path, $callback, $callback_method);
   }
   
-  private function addRoute($method, $path, $callback, $callback_method) {
+  private function route($method, $path, $callback, $callback_method) {
     if ($callback_method !== null) {
       $callback = array($callback, $callback_method);
     }
-    $route = new Route($method, $path, $callback);
-    $this->routes[] = $route;
-    return $route;
+    $this->routes[] = new Route($method, $path, $callback);
   }
   
   public function routes() {
