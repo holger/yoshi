c2d61d9b5b63fd8d44b8d69bb3238870f577c0fb
Holger Rüprich <holger@rueprich.de>
2012-04-21 00:25:32 +0200

    Added license info

--- src/yoshi/exceptions/NotFoundException.php
+++ src/yoshi/exceptions/NotFoundException.php
@@ -1,11 +1,14 @@
 <?php
-
+/**
+ * This file is part of Yoshi.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace yoshi\exceptions;
 
 use \Exception;
 
-class NotFoundException extends Exception {
-  
-}
+class NotFoundException extends Exception {}
 
 ?>
--- tests/yoshi/UrlHelperTest.php
+++ tests/yoshi/UrlHelperTest.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * This file is part of Yoshi.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace yoshi;
 
 class UrlHelperTest extends \PHPUnit_Framework_TestCase
